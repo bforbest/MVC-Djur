876e4ea674c2c510f4bf67940a502614dc54252d
ali sina <ali@mra.li>
2016-04-05 10:00:46 +0200

    mvcdjur

---
+++ MVC-Djur/MVC-Djur/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Djur.Startup))]
+namespace MVC_Djur
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
